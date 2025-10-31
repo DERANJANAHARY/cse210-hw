54c0573bc1939bbeb90c2c9555266a2e1179c58e
DERANJANAHARY <deranjanahary@gmail.com>
2025-10-31 06:52:29 +0300

    Practice-Exercise2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,11 +3,15 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   //Ask the user for the first name
         Console.WriteLine("What is your first name?");
         String first_name = Console.ReadLine();
+
+        //Ask the user for the last name
         Console.WriteLine("What is your last name?");
         String last_name = Console.ReadLine();
+
+        //Print the introducing phrase
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //Ask users for the grade
+        Console.WriteLine("Type grade percentage");
+        string gradeInput = Console.ReadLine();
+        string letter = "";
+        string sign = "";
+
+        int gradeValue = int.Parse(gradeInput);
+        int signValue = gradeValue % 10;
+
+        //Conditions on letters
+        if (gradeValue >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeValue >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeValue >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeValue >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradeValue < 60)
+        {
+            letter = "F";
+        }
+        // Determine the sign
+        if (signValue >= 7 && gradeValue < 93 && gradeValue > 60)
+        {
+            sign = "+";
+        }
+
+        else if (signValue < 3 && gradeValue < 93 && gradeValue > 60)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+            //Print grade letter
+            Console.WriteLine($"Your grade is {letter}{sign}");
+
+        //Check pass grade 
+        if (gradeValue >= 70)
+        {
+            Console.WriteLine("You pass. Congratulations!");
+        }
+
+        else if (gradeValue < 70)
+        {
+            Console.WriteLine("You have at least 70% to pass. You can work more and take the exam again.");
+        }
+
     }
 }
